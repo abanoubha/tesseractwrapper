b3a242f129dec1b65cc25ad28ce3068552935e34
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-03-03 13:00:40 +0200

    testing #2

--- tesseract_test.go
+++ tesseract_test.go
@@ -5,17 +5,18 @@ import "testing"
 func TestImageData_GetText(t *testing.T) {
 	testFiles := map[string]string{
 		"testFiles/1695.gif":               "1695",
-		"testFiles/alphabet.jpeg":          "1695",
-		"testFiles/elon-musk-bitcoin.webp": "1695",
+		"testFiles/alphabet.jpeg":          "a",
+		"testFiles/elon-musk-bitcoin.webp": "=",
 	}
-	image := NewImageData("testFiles/1695.gif", "eng", 70, 13, 3)
-	got, err := image.GetText()
-	want := "1695"
-	if err != nil {
-		t.Fatalf("get error %q", err)
-	}
-	if got != want {
-		t.Fatalf("want %q, get %q", want, got)
+	for imagePath, expected := range testFiles {
+		image := NewImageData(imagePath, "eng", 70, 13, 3)
+		got, err := image.GetText()
+		if err != nil {
+			t.Fatalf("get error %q", err)
+		}
+		if got != expected {
+			t.Fatalf("want %q, get %q", expected, got)
+		}
 	}
 }
 
@@ -23,13 +24,14 @@ func TestArabicImageData_GetText(t *testing.T) {
 	testFiles := map[string]string{
 		"testFiles/is-android-apps-profitable.jpg": "1695",
 	}
-	image := NewImageData("testFiles/1695.gif", "eng", 70, 13, 3)
-	got, err := image.GetText()
-	want := "1695"
-	if err != nil {
-		t.Fatalf("get error %q", err)
-	}
-	if got != want {
-		t.Fatalf("want %q, get %q", want, got)
+	for imagePath, expected := range testFiles {
+		image := NewImageData(imagePath, "ara", 70, 13, 3)
+		got, err := image.GetText()
+		if err != nil {
+			t.Fatalf("get error %q", err)
+		}
+		if got != expected {
+			t.Fatalf("want %q, get %q", expected, got)
+		}
 	}
 }
