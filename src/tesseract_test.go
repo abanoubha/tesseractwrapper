2f40091ac2286f970ae69d0da86edd257712c370
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-02-16 23:25:10 +0200

    fix: correct paths

--- src/tesseract_test.go
+++ src/tesseract_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestImageData_GetText(t *testing.T) {
 	testFiles := map[string]string{
-		"testFiles/1695.gif":               "1695",
-		"testFiles/alphabet.jpeg":          "a",
-		"testFiles/elon-musk-bitcoin.webp": "=",
+		"../testFiles/1695.gif":               "1695",
+		"../testFiles/alphabet.jpeg":          "a",
+		"../testFiles/elon-musk-bitcoin.webp": "=",
 	}
 	for imagePath, expected := range testFiles {
 		image := NewImageData(imagePath, "eng", 70, 13, 3)
@@ -22,7 +22,7 @@ func TestImageData_GetText(t *testing.T) {
 
 func TestArabicImageData_GetText(t *testing.T) {
 	testFiles := map[string]string{
-		"testFiles/is-android-apps-profitable.jpg": "1695",
+		"../testFiles/is-android-apps-profitable.jpg": "1695",
 	}
 	for imagePath, expected := range testFiles {
 		image := NewImageData(imagePath, "ara", 70, 13, 3)
